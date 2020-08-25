92ec217d04470e98b2e2409d15254a95470ba47c
ainsleyk <56616943+ainsleyk@users.noreply.github.com>
2020-08-24 21:33:09 -0700

    deletephp

--- php/callProxy.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include 'Fresh\touchdraw\AMP_Tool\cartoDBProxy.php';
-    //          ^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
-    $queryURL = $_POST['sql'];
-    $return = goProxy($queryURL);
-    echo $return;
-?>
